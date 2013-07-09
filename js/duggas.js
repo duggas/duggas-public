6c4e4a87cc6f3d36b3b531b942720e34efc2fe5e
Brad Dugdale <duggas@gmail.com>
2013-07-08 22:37:24 -0400

    Added placeholder and trim

--- js/duggas.js
+++ js/duggas.js
@@ -1,5 +1,56 @@
+
+
 var DUGGAS;
 
-(function(){
+(function(DUGGAS){
 
-})();
+    DUGGAS = {
+
+        trim: function(str) {
+            return $.trim(str);
+        },
+
+        Placeholder: function(elem, placeholderText, stopPropagation){
+            (function(elem, placeholderText){
+                $(elem).blur(function(event){
+                    var trimmedText = DUGGAS.trim($(this).attr("value"));
+                    if(trimmedText === '' || trimmedText ===  placeholderText){
+                        $(this).css('color', 'grey');
+                        $(this).css('font-style', 'italic');
+                        $(this).attr("value", placeholderText);
+                    }
+                    else {
+                        $(this).css('color', '#000000');
+                        $(this).css('font-style', 'normal');
+                    }
+
+                    if(stopPropagation){
+                        event.stopPropagation();
+                    }
+                });
+            })(elem, placeholderText);
+
+            (function(elem, placeholderText){
+                $(elem).click(function(event){
+                    var trimmedText = EM.trim($(this).attr("value"));
+                    if(trimmedText ===  placeholderText){
+                        //Clear it
+                        $(this).attr("value", "");
+                    }
+
+                    $(this).css('color', '#000000');
+                    $(this).css('font-style', 'normal');
+
+                    if(stopPropagation){
+                        event.stopPropagation();
+                    }
+
+                });
+            })(elem, placeholderText);
+
+            //Triggers the inital setting of the placeholder text, if needed
+            $(elem).trigger('blur');
+        }
+    }
+
+})(DUGGAS);
